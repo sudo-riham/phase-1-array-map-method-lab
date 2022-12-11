31ce5d2c93dc0fd6a0da0a6811ca8aad666ddc1d
rihamzeidan <r.zeidan@eweev.com>
2022-12-11 22:10:31 +0200

    Solution

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((sentence) => {
+    const words = sentence.split(" ");
+
+    const newSentence = words.map((word) => {
+      return word[0].toUpperCase() + word.substr(1);
+    });
+
+    return newSentence.join(" ");
+  });
+};
